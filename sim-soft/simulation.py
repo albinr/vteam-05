6e5e3c42304aa799e19a7cee7332bd206038c0be
Albin Ryberg <alry23@student.bth.se>
2024-11-29 17:15:42 +0100

    updated git ignore and added requriements.txt to sim-soft folder

--- sim-soft/simulation.py
+++ sim-soft/simulation.py
@@ -1,8 +1,52 @@
-from datetime import datetime
+# simulation.py
 
-class User:
-    def __init__(self, users, bikes):
+class Simulation:
+    """
+    A class to represent a scooter simulation environment.
+
+    Attributes:
+    -----------
+    users : int
+        The number of users participating in the simulation.
+    bikes : int
+        The number of bikes available in the simulation.
+
+    Methods:
+    --------
+    start():
+        Starts the simulation.
+    stop():
+        Stops the simulation.
+    """
+
+    def __init__(self, bikes, users=0):
+        """
+        Initializes the Simulation with a given number of bikes and users.
+
+        Parameters:
+        -----------
+        bikes : int
+            The number of bikes to include in the simulation.
+        users : int, optional
+            The number of users to include in the simulation (default is 0).
+        """
         self.users = users
         self.bikes = bikes
-    def start():
+
+    def start(self):
+        """
+        Starts the simulation.
+
+        This method should contain logic to initialize and begin
+        the simulation of scooters and users in the environment.
+        """
+        pass
+
+    def stop(self):
+        """
+        Stops the simulation.
+
+        This method should contain logic to end and clean up after
+        the simulation has finished running.
+        """
         pass
--- sim-soft/user.py
+++ sim-soft/user.py
@@ -2,16 +2,25 @@ import json, random, time, os
 from datetime import datetime
 
 class User:
-    def __init__(self, user_id, bike_id):
+    def __init__(self, user_id, name, bike_id):
         self.user_id = user_id
-        self.name = 100
-        self.location = {"latitude": 59.3293, "longitude": 18.0686}
+        self.name = name
         self.bike_id = bike_id
+        self.is_alive = True
 
     def update(self):
         pass
 
+    def register(self):
+        pass
+
+    def rentScooter(self):
+        pass
+
+    def returnScooter(self):
+        pass
+
     def run(self):
-        while self.name > 0:
+        while self.alive:
             self.update()
             time.sleep(5)
