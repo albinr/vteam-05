5db0006c19dfd3159f4c659260a4a2d25fcfb574
Tiqma <tiqmax@outlook.com>
2024-12-09 13:30:57 +0100

    just fixed backend again

--- backend/sql/test.sql
+++ backend/sql/test.sql
@@ -4,17 +4,16 @@
 
 USE vteam;
 
-source setup.sql
-source ddl.sql
+-- source setup.sql
+-- source ddl.sql
 
-INSERT INTO Bike (position)
-VALUES (POINT(16.1612, 55.5869));
-INSERT INTO Bike (position)
-VALUES (POINT(76.1612, 15.5869));
+
 
 -- SELECT ST_AsText(position) AS start_position FROM Bike;
 
-CALL StartTrip(1);
+-- CALL StartTrip(1);
+-- CALL EndTrip(1, POINT(16.1613, 55.5869));
+-- CALL EndTrip(2, POINT(16.1613, 55.5869));
 -- SELECT
 -- ST_X(position) AS position
 -- FROM Bike
@@ -31,7 +30,7 @@ CALL StartTrip(1);
 -- WHERE bike_id = 1;
 -- CALL LogBikeMovement(1, POINT(16.1615, 55.5869));
 
-CALL EndTrip(1, POINT(56.1612, 15.5869));
+-- CALL EndTrip(1, POINT(56.1612, 15.5869));
 -- CALL EndTrip(1, POINT(96.1612, 15.5869));
 -- CALL StartTrip(2);
 -- CALL EndTrip(2, POINT(86.1612, 15.5869));
@@ -44,17 +43,20 @@ bike_id,
 start_time,
 end_time,
 CONCAT (ST_X(start_position), ' ', ST_Y(start_position)) AS start_position,
-CONCAT (ST_X(end_position), ' ', ST_Y(end_position)) AS end_position,
-CONCAT (price, 'kr') AS price,
-speed
+CONCAT (ST_X(end_position), ' ', ST_Y(end_position)) AS end_position
 FROM Trip;
 
+-- SELECT
+-- movement_id,
+-- bike_id,
+-- ST_X(position),
+-- ST_Y(position)
+-- FROM BikeMovement
+-- ORDER BY bike_id, movement_id;
 
 SELECT
 bike_id,
 status,
-battery_level,
 ST_X(position),
 ST_Y(position)
-FROM Bike
-WHERE bike_id = 1;
+FROM Bike;
