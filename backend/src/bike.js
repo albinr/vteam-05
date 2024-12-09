42d409b546e6be86fe74857f0f46755f89d3e010
Marcus N Ahlin <marcusnahlin@gmail.com>
2024-12-09 16:04:51 +0100

    Getting config from docker compose instead of config-file.

--- backend/src/bike.js
+++ backend/src/bike.js
@@ -1,6 +1,17 @@
 const mysql = require('mysql2/promise');
 const config = require("../data/vteam.json");
-const pool = mysql.createPool(config);
+// const pool = mysql.createPool(config);
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    multipleStatements: true,
+    queueLimit: 0
+});
 
 process.on("exit", () => {
     pool.end();
