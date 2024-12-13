41883da0c6302245b893b9baab651ed7be2140f4
Marcus N Ahlin <marcusnahlin@gmail.com>
2024-12-13 11:46:36 +0100

    Added start off app, added map.

---
+++ frontend/user-app/app/(tabs)/index.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from "react-native";
+// import HomeScreen from "@/screens/HomeScreen";
+// import MapScreen from "@/screens/MapScreen";
+// import {NavigationContainer} from '@react-navigation/native';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+export default function Index() {
+
+  // const userLoggedIn: boolean = false;
+
+  return (
+      <View>
+        <Text>Index</Text>
+      </View>
+  );
+}
+
+// const styles = StyleSheet.create({
+// });
---
+++ frontend/user-app/app/(tabs)/map.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from "react-native";
+// import HomeScreen from "@/screens/HomeScreen";
+// import MapScreen from "@/screens/MapScreen";
+// import {NavigationContainer} from '@react-navigation/native';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import Map from "@/components/Map";
+// import LoginScreen from "@/screens/LoginScreen";
+import Map from "@/components/Map";
+
+export default function MapPage() {
+
+  return (
+      <Map />
+  );
+}
+
+// const styles = StyleSheet.create({
+// });
