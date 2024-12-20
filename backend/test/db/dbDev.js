ab96d41fc69e892a796a02b00c84774d073c5cdb
Tiqma <tiqmax@outlook.com>
2024-12-20 19:20:50 +0100

    testing actions

--- backend/test/db/dbDev.js
+++ backend/test/db/dbDev.js
@@ -1,29 +1,19 @@
 const mysql = require('mysql2/promise');
-const config = require('../../data/development.json');
 require('dotenv').config();
 
-const useDocker = process.env.USE_DOCKER === 'true';
 const isTestEnvironment = process.env.NODE_ENV === 'test';
-const database = isTestEnvironment ? 'testdb' : process.env.DB_NAME;
+const database = isTestEnvironment ? 'testdb' : process.env.DB_NAME || 'testdb';
 
-const pool = mysql.createPool(
-    useDocker
-        ? {
-              host: process.env.DB_HOST || 'localhost',
-              port: process.env.DB_PORT || 1387,
-              user: process.env.DB_USER || 'root',
-              password: process.env.DB_PASSWORD || '',
-              database,
-              waitForConnections: true,
-              connectionLimit: 10,
-              multipleStatements: true,
-              queueLimit: 0,
-          }
-        : {
-              ...config,
-              database,
-          }
-);
+const pool = mysql.createPool({
+    host: process.env.DB_HOST || 'Timpa.local',
+    user: process.env.DB_USER || 'dbadm',
+    password: process.env.DB_PASSWORD || 'P@ssw0rd',
+    database,
+    multipleStatements: true,
+    connectionLimit: 10,
+    waitForConnections: true,
+    queueLimit: 0,
+});
 
 process.on('exit', () => {
     pool.end();
