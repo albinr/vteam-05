ecefd47a22e7d6745c779eae3f3d2f894fdb5bf1
Tiqma <tiqmax@outlook.com>
2024-12-28 23:18:27 +0100

    coverage test

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     collectCoverage: true,
     coverageDirectory: "coverage", // Var täckningsrapporten sparas
-    coverageReporters: ["text", "lcov"], // Format för täckningsrapport
+    coverageReporters: ["text", "clover"], // Format för täckningsrapport
     testEnvironment: "node", // Miljö som Jest använder
   };
