fcfba6ae03a96a30438b2b7f1b7c28547ad2c694
Albin Ryberg <alry23@student.bth.se>
2025-01-28 23:55:43 +0100

    testing out vitest to avoid deprecated dependencies

--- frontend/admin-web/tests/example.test.js
+++ frontend/admin-web/tests/example.test.js
@@ -1,3 +1,7 @@
-test('adds 1 + 1 to equal 2', () => {
-    expect(1 + 1).toBe(2);
+import { describe, it, expect } from 'vitest';
+
+describe('Addition', () => {
+    it('adds 1 + 1 to equal 2', () => {
+        expect(1 + 1).toBe(2);
+    });
 });
